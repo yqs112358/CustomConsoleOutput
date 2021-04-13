9a477eae286d634799d8111ff33c1034f0dae2f7
Qi You <1425321705@qq.com>
2021-04-13 21:08:57 +0800

    Update notes

--- Customize.cpp
+++ Customize.cpp
@@ -2,15 +2,14 @@
 
 bool OutputProcess(std::string& line)
 {
-	
+	// Every time when a line is to output, this function will be called
+	//     to process, check or modify it.
+	// You can even stop it from output when necessary.
+	// 
+	// Add your code here to process the output.
+	// 	   return false - if you don't want this line to be output
+	// 
+	// Note that string "line" is passed by quote
+
 	return true;
 }
-// Every time when a line is to output, this function will be called
-//     to process, check or modify it,
-//     even to stop it from output when necessary.
-// 
-// Add your code here to process the output.
-// 	   return true  - if you want to output the line to console
-// 	   return false - if you don't want this line to be output
-// 
-// Note that string "line" is passed by quote
